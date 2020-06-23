2a511dc1ef7aaaa64c59e3e93f317e08a6887c41
pisalore <lorenzo.pisaneschi@gmail.com>
2020-06-23 10:56:51 +0200

    filtering provinces

--- Floyd-Warshall _ClusteringCoeff.py
+++ Floyd-Warshall _ClusteringCoeff.py
@@ -1,6 +1,20 @@
 import json
+import networkx as nx
 
 with open('dpc-covid19-ita-province.json') as f:
-    data = json.load(f)
+    provinces = json.load(f)
 
-    print(data)
+# Provinces graph
+P = nx.Graph()
+# filter reference date
+reference_date = provinces[0]['data']
+all_provinces = [provinces[0]]
+
+# filtering provinces using date and 'denominazione_provincia'
+for province in (y for y in provinces[1:] if y['denominazione_provincia'] != 'In fase di definizione/aggiornamento'):
+    if province['data'] == reference_date:
+        all_provinces.append(province)
+    else:
+        break
+
+print(all_provinces)
