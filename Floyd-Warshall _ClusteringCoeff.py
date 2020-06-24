f240a1fbb6362b6972d9b31075074473d3e05398
pisalore <lorenzo.pisaneschi@gmail.com>
2020-06-24 17:01:09 +0200

    added weights to edges

--- Floyd-Warshall _ClusteringCoeff.py
+++ Floyd-Warshall _ClusteringCoeff.py
@@ -2,13 +2,15 @@ import json
 import networkx as nx
 import random
 import pylab as plt
+import math
 
 D1 = 0.8
 D2 = 0.08
 R_NUM_NODES = 2000
 
-with open('dpc-covid19-ita-province.json') as f:
-    json_provinces = json.load(f)
+
+def euclidean_distance(x1, y1, x2, y2):
+    return math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
 
 
 def construct_graph(provinces, threshold):
@@ -31,7 +33,8 @@ def construct_graph(provinces, threshold):
         for b in (n for n in graph.nodes(data=True) if (n[0] > index and n != a)):
             if (a[1]['long'] - threshold < b[1]['long'] < a[1]['long'] + threshold) \
                     and (a[1]['lat'] - threshold < b[1]['lat'] < a[1]['lat'] + threshold):
-                graph.add_edge(a[0], b[0], a=a[1]['city'], b=b[1]['city'])
+                graph.add_edge(a[0], b[0], a=a[1]['city'], b=b[1]['city'],
+                               weight=euclidean_distance(a[1]['long'], a[1]['lat'], b[1]['long'], b[1]['lat']))
                 print('Città: ' + a[1]['city'] + ' longitudine: ', a[1]['long'], 'latitudine: ', a[1]['lat'],
                       'Città: ' + b[1]['city'] + ' longitudine: ', b[1]['long'], 'latitudine: ', b[1]['lat'])
     return graph
@@ -47,17 +50,24 @@ def construct_random_graph(nodes_num, x_inf, x_sup, y_inf, y_sup, threshold):
         for b in (n for n in graph.nodes(data=True) if (n[0] > index and n != a)):
             if (a[1]['x'] - threshold < b[1]['x'] < a[1]['x'] + threshold) \
                     and (a[1]['y'] - threshold < b[1]['y'] < a[1]['y'] + threshold):
-                graph.add_edge(a[0], b[0])
+                graph.add_edge(a[0], b[0], weight=euclidean_distance(a[1]['x'], a[1]['y'], b[1]['x'], b[1]['y']))
                 print(' x: ', a[1]['x'], 'y: ', a[1]['y'],
                       ' x: ', b[1]['x'], 'y: ', b[1]['y'])
     return graph
 
 
+def floyd_warshall(graph):
+    print()
+
+
+with open('dpc-covid19-ita-province.json') as f:
+    json_provinces = json.load(f)
+
 print('P')
 P = construct_graph(json_provinces, D1)
 print('R')
 R = construct_random_graph(R_NUM_NODES, 30, 51, 10, 19, 0.08)
-# to draw graph. TODO:use graphviz
-nx.draw(P)
-plt.show()
 
+# to draw graph. TODO:use graphviz
+# nx.draw(P)
+# plt.show()
