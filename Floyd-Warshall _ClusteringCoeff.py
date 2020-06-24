c8ce1ef4e38c91df8ab7a7255781c0da66ecfa3a
pisalore <lorenzo.pisaneschi@gmail.com>
2020-06-24 17:37:31 +0200

    floyd-warshall algorithm init

--- Floyd-Warshall _ClusteringCoeff.py
+++ Floyd-Warshall _ClusteringCoeff.py
@@ -3,10 +3,12 @@ import networkx as nx
 import random
 import pylab as plt
 import math
+import numpy as np
 
 D1 = 0.8
 D2 = 0.08
 R_NUM_NODES = 2000
+INF = 99999999
 
 
 def euclidean_distance(x1, y1, x2, y2):
@@ -37,6 +39,8 @@ def construct_graph(provinces, threshold):
                                weight=euclidean_distance(a[1]['long'], a[1]['lat'], b[1]['long'], b[1]['lat']))
                 print('Città: ' + a[1]['city'] + ' longitudine: ', a[1]['long'], 'latitudine: ', a[1]['lat'],
                       'Città: ' + b[1]['city'] + ' longitudine: ', b[1]['long'], 'latitudine: ', b[1]['lat'])
+            # else:
+            #    graph.add_edge(a[0], b[0], a=a[1]['city'], b=b[1]['city'], weight=INF)
     return graph
 
 
@@ -51,23 +55,28 @@ def construct_random_graph(nodes_num, x_inf, x_sup, y_inf, y_sup, threshold):
             if (a[1]['x'] - threshold < b[1]['x'] < a[1]['x'] + threshold) \
                     and (a[1]['y'] - threshold < b[1]['y'] < a[1]['y'] + threshold):
                 graph.add_edge(a[0], b[0], weight=euclidean_distance(a[1]['x'], a[1]['y'], b[1]['x'], b[1]['y']))
-                print(' x: ', a[1]['x'], 'y: ', a[1]['y'],
-                      ' x: ', b[1]['x'], 'y: ', b[1]['y'])
+                print(' x: ', a[1]['x'], 'y: ', a[1]['y'], ' x: ', b[1]['x'], 'y: ', b[1]['y'])
+            # else:
+            #    graph.add_edge(a[0], b[0], weight=INF)
     return graph
 
 
+# Floyd Warshall Algorithm
 def floyd_warshall(graph):
-    print()
-
+    # init a full n*n np array (n = nodes number)
+    n = graph.number_of_nodes()
+    A = np.full((n, n), INF)
+    print(A.shape)
 
 with open('dpc-covid19-ita-province.json') as f:
     json_provinces = json.load(f)
 
 print('P')
 P = construct_graph(json_provinces, D1)
-print('R')
-R = construct_random_graph(R_NUM_NODES, 30, 51, 10, 19, 0.08)
+# print('R')
+# R = construct_random_graph(R_NUM_NODES, 30, 51, 10, 19, 0.08)
 
+floyd_warshall(P)
 # to draw graph. TODO:use graphviz
 # nx.draw(P)
 # plt.show()
