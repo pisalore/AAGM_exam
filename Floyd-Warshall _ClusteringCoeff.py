0038de73b9aa7f47fcc847283f3382f73a89ffd1
pisalore <lorenzo.pisaneschi@gmail.com>
2020-07-14 09:50:44 +0200

    bug fixed

--- Floyd-Warshall _ClusteringCoeff.py
+++ Floyd-Warshall _ClusteringCoeff.py
@@ -72,7 +72,8 @@ def construct_provinces_graph(provinces):
     reference_date = provinces[0]['data']
     # filtering provinces using date and 'denominazione_provincia'
     province_id = 0
-    for province in (y for y in provinces if y['denominazione_provincia'] != 'In fase di definizione/aggiornamento'):
+    for province in (y for y in provinces if
+                     y['denominazione_provincia'] != 'In fase di definizione/aggiornamento'):
         if province['data'] == reference_date:
             graph.add_node(province_id, city=province['denominazione_provincia'], long=province['long'],
                            lat=province['lat'])
@@ -93,14 +94,14 @@ def construct_random_graph(nodes_num, x_inf, x_sup, y_inf, y_sup):
 # add edges if edge near criteria is satisfied O(n^2)
 def set_provinces_edges_expensive(graph, threshold):
     start_expensive = time.time()
+    index = 0
     for a in graph.nodes(data=True):
-        for b in graph.nodes(data=True):
+        index += 1
+        for b in (n for n in graph.nodes(data=True) if (n[0] > index and n != a)):
             if (a[1]['long'] - threshold < b[1]['long'] < a[1]['long'] + threshold) \
                     and (a[1]['lat'] - threshold < b[1]['lat'] < a[1]['lat'] + threshold):
                 graph.add_edge(a[0], b[0], a=a[1]['city'], b=b[1]['city'],
                                weight=euclidean_distance(a[1]['long'], a[1]['lat'], b[1]['long'], b[1]['lat']))
-                # print('Città: ' + a[1]['city'] + ' longitudine: ', a[1]['long'], 'latitudine: ', a[1]['lat'],
-                #       'Città: ' + b[1]['city'] + ' longitudine: ', b[1]['long'], 'latitudine: ', b[1]['lat'])
     print('"Expensive method" provinces graph:', time.time() - start_expensive)
     return graph
 
